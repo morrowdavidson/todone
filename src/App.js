a78eca3935e9f23be3b9a963e62d20d11998fff6
Morrow Davidson <morrow.davidson@gmail.com>
2019-03-07 09:41:43 -0800

    Add header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="page">
+        <div id="app-container">
+          <div id="header">
+            <div id="logo_tag">
+              <h1>todone</h1>
+              <p>A simple todo app.</p>
+            </div>
+            <i className="gear-icon"></i>
+          </div>
+        </div>
       </div>
     );
   }
