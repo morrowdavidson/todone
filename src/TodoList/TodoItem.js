b4385696635ab738af67a45b1db7b0b9b2868304
Morrow Davidson <morrow.davidson@gmail.com>
2019-03-09 10:26:50 -0800

    Add delete button view

--- src/TodoList/TodoItem.js
+++ src/TodoList/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const todoItem = (props) => {
     return(
-        <li className="list-group-item"><i className="far fa-circle"></i> <span>{props.content}</span></li>
+        <li className="list-group-item"><i className="far fa-circle"></i> <span>{props.content} <i class="far fa-trash-alt"></i></span></li>
     )
 }
 
