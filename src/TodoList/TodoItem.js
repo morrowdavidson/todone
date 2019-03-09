00d1c5adc9c22a19e6d22c159c4c77d775b75eb3
Morrow Davidson <morrow.davidson@gmail.com>
2019-03-09 01:39:18 -0800

    Tweek style

--- src/TodoList/TodoItem.js
+++ src/TodoList/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const todoItem = (props) => {
     return(
-        <li className="list-group-item"><i className="far fa-check-circle"></i> {props.content}</li>
+        <li className="list-group-item"><i className="far fa-circle"></i> {props.content}</li>
     )
 }
 
