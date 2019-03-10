0e3fd802db35348790700c260d66f4ffbbe9ad91
Morrow Davidson <morrow.davidson@gmail.com>
2019-03-10 03:18:39 -0700

    Refactor

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -53,7 +53,7 @@ class TodoList extends Component {
           ...currentState,
           {
             content: event.target.value,
-            key: this.state.keyCounter,
+            key: this.state.keyCounter + [listId],
             isChecked: false,
           }
         ]
@@ -70,9 +70,7 @@ class TodoList extends Component {
     }
   }
   
-  removeItem = (event) => {
-    let listId = event.target.attributes['data'].value;
-    let key = parseInt(event.target.id);
+  removeItem = (listId, key) => {
     let currentState = this.state[listId];
     let newState = currentState.filter((listState)=>{
       return listState.key !== key
@@ -84,9 +82,7 @@ class TodoList extends Component {
 
   }
 
-  checkedToggle = (event) => {
-    let listId = event.target.attributes['data'].value;
-    let key = parseInt(event.target.id);
+  checkedToggle = (event, key, listId) => {
     let currentState = this.state[listId];
 
     if(event.target.className === 'far fa-circle'){
@@ -116,9 +112,7 @@ class TodoList extends Component {
     }
   }
 
-  moveItem = (event) => {
-    let listId = event.target.attributes['data'].value;
-    let key = parseInt(event.target.id);
+  moveItem = (key, listId) => {
     let currentState = this.state[listId];
     let addedItem = currentState.filter((listState)=>{
       return listState.key === key
@@ -150,36 +144,34 @@ class TodoList extends Component {
   render(){
     let allItems = this.state.allList.map((listItem) => {
       return <TodoItem 
-              checkedToggle={this.checkedToggle} 
+              checkedToggle={(event) => {this.checkedToggle(event, listItem.key, "allList")}} 
               checkedClass={listItem.isChecked ? 'fa-check-circle' : 'fa-circle'} 
               id="allList" 
               key={listItem.key} 
-              keyValue={listItem.key} 
               content={listItem.content} 
-              removeItem={this.removeItem}
-              quickLinks={<div className="quicklinks"><a data="allList" onClick={this.moveItem} id={listItem.key + 'MoveWeek'} href='#week'>Move to this week</a></div>}
+              removeItem={() => this.removeItem("allList", listItem.key)}
+              quickLinks={<div className="quicklinks"><a onClick={() => {this.moveItem(listItem.key, "allList")}} id={listItem.key + 'MoveWeek'} href='#week'>Move to this week</a></div>}
               />
     }) 
     let weekItems = this.state.weekList.map((listItem) => {
       return <TodoItem 
-              checkedToggle={this.checkedToggle} 
+              checkedToggle={(event) => {this.checkedToggle(event, listItem.key, "weekList")}} 
               checkedClass={listItem.isChecked ? 'fa-check-circle' : 'fa-circle'} 
               id="weekList" 
               key={listItem.key} 
-              keyValue={listItem.key} 
               content={listItem.content} 
-              removeItem={this.removeItem}
-              quickLinks={<div className="quicklinks"><a data="weekList" onClick={this.moveItem} id={listItem.key + 'MoveToday'} href='#today'>Move to today</a></div>}
+              removeItem={() => this.removeItem("weekList", listItem.key)}
+              quickLinks={<div className="quicklinks"><a onClick={() => {this.moveItem(listItem.key, "weekList")}} id={listItem.key + 'MoveToday'} href='#today'>Move to today</a></div>}
               />
     }) 
     let todayItems = this.state.todayList.map((listItem) => {
       return <TodoItem 
-              checkedToggle={this.checkedToggle} 
+              checkedToggle={(event) => {this.checkedToggle(event, listItem.key, "todayList")}} 
               checkedClass={listItem.isChecked ? 'fa-check-circle' : 'fa-circle'} 
-              id="todayList" key={listItem.key} 
-              keyValue={listItem.key} 
+              id="todayList" 
+              key={listItem.key} 
               content={listItem.content} 
-              removeItem={this.removeItem}
+              removeItem={() => this.removeItem("todayList", listItem.key)}
               />
     }) 
     
