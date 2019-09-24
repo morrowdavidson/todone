48e5bffef8fea0b40615a878eb5ddbe5fcd32e3f
Morrow Davidson <morrow.davidson@gmail.com>
2019-09-24 12:43:43 -0500

    Update todone

---
+++ todone/precache-manifest.8665a6b012e634b62baa566888bed997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91e12a64480111cd0d2d870881fe9902",
+    "url": "/todone/static/media/todone_logo.91e12a64.svg"
+  },
+  {
+    "revision": "f25b4c0b93513826912d",
+    "url": "/todone/static/js/runtime~main.e60d8ae5.js"
+  },
+  {
+    "revision": "66cf3a3394f3c8f77755",
+    "url": "/todone/static/js/main.4150aaf3.chunk.js"
+  },
+  {
+    "revision": "c69ca85d526c3c87f830",
+    "url": "/todone/static/js/2.609099d6.chunk.js"
+  },
+  {
+    "revision": "66cf3a3394f3c8f77755",
+    "url": "/todone/static/css/main.808e5518.chunk.css"
+  },
+  {
+    "revision": "22b0d0ee2262a32c609cb7f78a1dff63",
+    "url": "/todone/index.html"
+  }
+];
