ec76efe1c2deae5ef839abc9da8878ac3a0d39c6
Morrow Davidson <morrow.davidson@gmail.com>
2019-09-24 23:44:06 -0500

    Add build folder

---
+++ build/precache-manifest.06ba407b8237b709414c288f35539f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "91e12a64480111cd0d2d870881fe9902",
+    "url": "/static/media/todone_logo.91e12a64.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "60903992a95072e5685e",
+    "url": "/static/js/main.94aba461.chunk.js"
+  },
+  {
+    "revision": "a3e3ef9fe450b5ef3759",
+    "url": "/static/js/2.519d7ee8.chunk.js"
+  },
+  {
+    "revision": "60903992a95072e5685e",
+    "url": "/static/css/main.62b134c3.chunk.css"
+  },
+  {
+    "revision": "27daa56a3b694db62e128d963bd7baa1",
+    "url": "/index.html"
+  }
+];
